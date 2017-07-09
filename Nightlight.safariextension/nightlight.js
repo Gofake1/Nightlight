Gofake1/Nightlight

commit 90440b778e42f4be96fec12595db9840f82b6564

Fix crash if aggressive list or blacklist are empty
Fix error when drawing color grid

Nightlight.safariextension/popover.html
@@ -49,9 +49,15 @@
 <div id="warning"></div>
 <div id="colorGrid"></div>
 <script>
-var AGGRESSIVE_LIST = safari.extension.settings.aggressiveList.split(',');
+var AGGRESSIVE_LIST = [];
+if (safari.extension.settings.aggressiveList !== undefined) {
+  safari.extension.settings.aggressiveList.split(',');
+}
 var AGGRESSIVE_TABS = new Set();
-var BLACKLIST       = safari.extension.settings.blacklist.split(',');
+var BLACKLIST       = [];
+if (safari.extension.settings.blacklist !== undefined) {
+  safari.extension.settings.blacklist.split(',');
+}
 var IGNORED_TABS    = new Set();
 var WARNING = {
   NOT_WEBPAGE: "This is not a webpage and won't be modified.",
@@ -95,6 +101,9 @@ function renderWarning(message) {
 }
 
 function renderColorGrid(colors) {
+  if (colors === null) {
+    return;
+  }
   while (ColorGrid.firstChild) {
     ColorGrid.removeChild(ColorGrid.firstChild);
   }

commit 7d430763a14f40ac3de5ba89a7ecaa2099401bc8

Fixed aggressive list and blacklist
Fixes regression introduced in 90440b77

Nightlight.safariextension/nightlight.js
@@ -51,7 +51,7 @@ function Color(r, g, b, a) {
   this.isTransparent = function() {
     return this.a === 0;
   };
-  /// Return saturation from HSV color model
+  /// Returns saturation from HSV color model
   this.saturation = function() {
     var max = Math.max(this.r, Math.max(this.g, this.b));
     var min = Math.min(this.r, Math.min(this.g, this.b));
@@ -144,12 +144,10 @@ function makeWebpageColors(element) {
   ) {
     if (oldBgColor.isColorful()) {
       newBgColor = oldBgColor.shade(-20);
+    } else if (oldBgColor.isLight()) {
+      newBgColor = oldBgColor.invert();
     } else {
-      if (oldBgColor.isLight()) {
-        newBgColor = oldBgColor.invert();
-      } else {
-        newBgColor = oldBgColor.shade(-50);
-      }
+      newBgColor = oldBgColor.shade(-50);
     }
     WEBPAGE_COLORS.bgColors.set(
       oldBgColor.rgbString(), newBgColor.rgbString()
@@ -234,7 +232,7 @@ function nightlight(mode, element) {
         }
         element.style.backgroundImage = 'none';
         element.style.setProperty('background-color', newBgColor.rgbString(), 'important');
-        element.style.color = newTextColor.rgbString();
+        element.style.setProperty('color', newTextColor.rgbString(), 'important');
         break;
       case 'CANVAS':
         if (IS_AGGRESSIVE) {
@@ -242,7 +240,6 @@ function nightlight(mode, element) {
         }
         break;
       case 'IMG':
-        element.style.backgroundColor = GrayColor();
         if (DARKEN_IMAGES) {
           element.style.filter = 'brightness(70%)';
         }
@@ -257,13 +254,12 @@ function nightlight(mode, element) {
       case 'DIV':
       case 'SPAN':
         if (IS_AGGRESSIVE) {
-          if (style.borderWidth != '0px') {
-            element.style.borderColor = newTextColor.rgbString();
-          }
+          element.style.borderWidth = '0px';
         } else if (style.backgroundImage != 'none') {
           return;
         }
         element.style.setProperty('background-color', newBgColor.rgbString(), 'important');
+        element.style.borderColor = newTextColor.rgbString();
         if (!newBgColor.isLight()) {
           element.style.color = newTextColor.rgbString();
         }

Nightlight.safariextension/popover.html
@@ -37,12 +37,12 @@
 </head>
 
 <body>
-<input id="isOnCheckbox" type="checkbox" onchange="handleIsOn(this)">Nightlight <span id="nightlightStatus" class="gray"></span><br>
+<input id="isOnCheckbox" type="checkbox" tabindex="-1" onchange="handleIsOn(this)">Nightlight <span id="nightlightStatus" class="gray"></span><br>
 <div id="moreSettingsContainer">
-  <button id="moreSettingsButton" class="blue centered textButton" onclick="handleMoreSettingsVisibility()">More Settings</button>
+  <button id="moreSettingsButton" class="blue centered textButton" tabindex="-1" onclick="handleMoreSettingsVisibility()">More Settings</button>
   <div id="moreSettings">
-    <input id="ignoreTabCheckbox" type="checkbox" onchange="handleIgnoreTab(this)"><span id="ignoreTabLabel">Ignore Tab</span><br>
-    <button id="aggressiveButton" class="red centered textButton" onclick="handleAggressive()">Aggressive</button><br>
+    <input id="ignoreTabCheckbox" type="checkbox" tabindex="-1" onchange="handleIgnoreTab(this)"><span id="ignoreTabLabel">Ignore Tab</span><br>
+    <button id="aggressiveButton" class="red centered textButton" tabindex="-1" onclick="handleAggressive()">Aggressive</button><br>
   </div>
 </div>
 <hr>
@@ -51,14 +51,15 @@
 <script>
 var AGGRESSIVE_LIST = [];
 if (safari.extension.settings.aggressiveList !== undefined) {
-  safari.extension.settings.aggressiveList.split(',');
+  AGGRESSIVE_LIST = safari.extension.settings.aggressiveList.split(',');
 }
-var AGGRESSIVE_TABS = new Set();
 var BLACKLIST       = [];
 if (safari.extension.settings.blacklist !== undefined) {
-  safari.extension.settings.blacklist.split(',');
+  BLACKLIST = safari.extension.settings.blacklist.split(',');
 }
+var AGGRESSIVE_TABS = new Set();
 var IGNORED_TABS    = new Set();
+var TAB_COLORS      = {};
 var WARNING = {
   NOT_WEBPAGE: "This is not a webpage and won't be modified.",
   BLACKLISTED: "This page is blacklisted and won't be modified.",
@@ -101,11 +102,11 @@ function renderWarning(message) {
 }
 
 function renderColorGrid(colors) {
-  if (colors === null) {
-    return;
-  }
   while (ColorGrid.firstChild) {
     ColorGrid.removeChild(ColorGrid.firstChild);
+  }
+  if (colors === null) {
+    return;
   }
 
   var colorTable = document.createElement('table');