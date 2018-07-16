bb92dad93be095f1ed8a84db460ebaa9e4f4579c
David Wu <david@gofake1.net>
2018-07-16 10:04:28 -0700

    Starting over

---
+++ Nightlight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nightlight
+//
+//  Created by David Wu on 7/16/18.
+//  Copyright © 2018 Gofake1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ SafariExtension/SafariExtensionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SafariExtensionViewController.swift
+//  SafariExtension
+//
+//  Created by David Wu on 7/16/18.
+//  Copyright © 2018 Gofake1. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
